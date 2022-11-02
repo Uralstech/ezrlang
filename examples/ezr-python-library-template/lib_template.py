349409b343f324fa90e61bf70571d9a0ae7ae4cc
Uralstech <uralstech@gmail.com>
2022-11-02 09:56:12 +0530

    📄 Updated lib_template.py with base_libObject!

--- examples/ezr-python-library-template/lib_template.py
+++ examples/ezr-python-library-template/lib_template.py
@@ -1,5 +1,6 @@
 # Import needed classes from ezr.py
-from ezr import Interpreter, RuntimeResult, RuntimeError, VarAccessNode, CallNode, BaseFunction, Nothing, Number, String, RTE_INCORRECTTYPE
+from ezr import RuntimeResult, RuntimeError, Nothing, Number, String, RTE_INCORRECTTYPE
+from Libraries.base.base_libObject import base_libObject
 
 # ezr value classes
 # -----------------
@@ -23,7 +24,7 @@ from ezr import Interpreter, RuntimeResult, RuntimeError, VarAccessNode, CallNod
 #   Function(name, body_node, arg_names, should_auto_return) -> Function definition
 #   Object(name, body_node, arg_names) -> Object definition
 #
-# NOTE: Dictionary keys must be hashable
+# NOTE: Dictionary keys must be hashable.
 # The Value and BaseFunction class are ONLY for inheritance, all others can be returned as output.
 # Refer to ezr.py for the code for these classes.
 
@@ -42,8 +43,8 @@ from ezr import Interpreter, RuntimeResult, RuntimeError, VarAccessNode, CallNod
 # 
 # Error tags are str values used to catch errors in the TRY statement.
 # An error tag can be as simple as this: EXAMPLE_ERRORTAG = 'EXAMPLE-ERRORTAG'
-# Any errors thrown with this tag will be caught by TRY statements using the 'EXAMPLE-ERRORTAG' tag
-# ezr.py has some builtin error tags too- they can be imported into any project
+# Any errors thrown with this tag will be caught by TRY statements using the 'EXAMPLE-ERRORTAG' tag.
+# ezr.py has some builtin error tags too- they can be imported into any project:
 #   RTE_DEFAULT        = 'RUNTIME'
 #   RTE_CUSTOM         = 'CUSTOM'
 #   RTE_DICTKEY        = 'DICTIONARY-KEY'
@@ -55,118 +56,21 @@ from ezr import Interpreter, RuntimeResult, RuntimeError, VarAccessNode, CallNod
 #   RTE_INCORRECTTYPE  = 'INCORRECT-TYPE'
 #   RTE_MATH		   = 'MATH'
 #   RTE_IO	   		   = 'IO'
-# More details about these error tags are available in the ezr documentation
+# More details about these error tags are available in the ezr documentation.
+#
+# ezr library folder
+# ------------------
+# When making a library for ezr, it should be put in the 'Libraries' folder in ezrShells' installed directory.
+# There, you'll find 'base_libObject.py' under the 'base' folder. This helps retrieve variables, call functions
+# and handle ObjectCallNodes. Use base_libObject as the parent for the main lib_object class.
 
 # Create main object
 # The name HAS to be 'lib_Object', otherwise ezr will show an error
-class lib_Object(BaseFunction):
+class lib_Object(base_libObject):
     # Initialization
     def __init__(self, internal_context=None):
         # Replace 'lib_template' with your librarys' name
-        super().__init__('lib_template')
-        
-        # Set the internal context
-        self.internal_context = internal_context
-
-    # Populating the arguments for function calls
-    def populate_args(self, arg_names, args, context):
-        res = RuntimeResult()
-        interpreter = Interpreter()
-
-        for i in range(len(args)):
-            arg_name = arg_names[i]
-            arg_value = res.register(interpreter.visit(args[i], self.internal_context))
-            if res.should_return(): return res
-
-            arg_value.set_context(context)
-            context.symbol_table.set(arg_name, arg_value)
-        return res.success(Nothing())
-    
-    # Checking no. of args and populating them
-    def check_and_populate_args(self, arg_names, args, context):
-        res = RuntimeResult()
-        res.register(self.check_args(arg_names, args))
-        if res.should_return(): return res
-        res.register(self.populate_args(arg_names, args, context))
-        if res.should_return(): return res
-        return res.success(Nothing())
-    
-    # Function called at initialization
-    def execute(self):
-        res = RuntimeResult()
-        
-        # Generate internal context
-        self.internal_context = self.generate_context()
-
-        # Call the initialize function
-        res.register(self.initialize(self.internal_context))
-
-        # Return if any error
-        if res.should_return(): return res
-
-        # Return a copy of self
-        return res.success(self.copy())
-    
-    # Function to 'retrieve' variables / functions from object
-    def retrieve(self, node):
-        res = RuntimeResult()
-        
-        # For VarAccessNodes (Variable access)
-        if isinstance(node, VarAccessNode):
-            # Retrieving variable from object, defaulting to NOTHING
-            return_value = self.get_variable(node.var_name_token.value)
-
-        # For CallNodes (Function / object calls)
-        elif isinstance(node, CallNode):
-            # Getting function name
-            method_name = f'function_{node.node_to_call.var_name_token.value}'
-            
-            # Retrieving function from object, defaulting to NOTHING
-            method = getattr(self, method_name, Nothing())
-
-            # If method is not NOTHING
-            if not isinstance(method, Nothing):
-                # Check and populate args
-                res.register(self.check_and_populate_args(method.arg_names, node.arg_nodes, self.internal_context))
-                
-                # Return if any error
-                if res.should_return(): return res
-
-                # Call the function itself
-                return_value = res.register(method(node, self.internal_context))
-
-                # Return if any error
-                if res.should_return(): return res
-
-            # If method is NOTHING, the return value is also NOTHING
-            else: return_value = method
-        
-        # If any other node is given, raise an error
-        else: raise Exception(f'Unknown node type {type(node).__name__}!')
-
-        # Return the value
-        return res.success(return_value.set_context(self.context).set_pos(node.start_pos, node.end_pos))
-
-    # Setting variable in internal context
-    def set_variable(self, name, value):
-        self.internal_context.symbol_table.set(name, value)
-
-    # Getting variable from internal context
-    def get_variable(self, name):
-        var_ = self.internal_context.symbol_table.get(name)
-        return var_ if var_ else Nothing()
-
-    # Copying the object
-    def copy(self):
-        copy = lib_Object(self.internal_context)
-        copy.set_pos(self.start_pos, self.end_pos)
-        copy.set_context(self.context)
-
-        return copy
-
-    # String representation of the object
-    def __repr__(self):
-        return f'<object {self.name}>'
+        super().__init__('lib_template', internal_context)
 
     # Start of your code
 
